39e71d4667e206838ea14856dfa2a06b523bc915
Victor Vimbert-Guerlais <victor.vimbert-guerlais@bginette.fr>
2025-04-29 09:38:54 +0200

    reorganisation

---
+++ examples/top module example/display.v
@@ -0,0 +1,52 @@
+module display(
+        input clk, 
+        input[15:0] in,
+        output reg [3:0] io_select,
+        output[7:0] io_segment
+    );
+    
+    reg[14:0] counter;
+    reg[1:0] selected;
+    reg[3:0] current_digit;
+    
+    m_hex_decoder decoder(.in(current_digit), .out(io_segment));
+    
+    always @ (posedge clk) begin // @suppress "Behavior-specific 'always' should be used instead of general purpose 'always'"
+        if (counter[14]) begin
+            counter[14:0] <= 15'b0;
+            selected <= selected + 1;
+        end
+        else begin 
+            counter <= counter + 1;
+        end
+    end
+    
+         
+    always @ (selected) begin // @suppress "Behavior-specific 'always' should be used instead of general purpose 'always'"
+        case(selected)
+            2'b00 : begin 
+                io_select = 4'b1110; 
+                current_digit <= in[3:0];
+                end
+                
+            2'b01 : begin 
+                io_select = 4'b1101; 
+                current_digit <= in[7:4];
+                end
+                
+            2'b10 : begin 
+                io_select = 4'b1011; 
+                current_digit <= in[11:8];
+                end
+                
+            2'b11 : begin 
+                io_select = 4'b0111; 
+                current_digit <= in[15:12];
+                end
+                
+            default : begin 
+                io_select = 4'b1111; 
+                end
+        endcase 
+    end
+endmodule
---
+++ examples/top module example/hex_decoder.v
@@ -0,0 +1,29 @@
+module m_hex_decoder(
+    input [3:0] in,
+    output reg [7:0] out
+    );
+    
+    always @ (in) // @suppress "Behavior-specific 'always' should be used instead of general purpose 'always'"
+    begin
+        case (in)
+            4'h0 : out = 8'b11000000; // '0'
+            4'h1 : out = 8'b11111001; // '1'
+            4'h2 : out = 8'b10100100; // '2'
+            4'h3 : out = 8'b10110000; // '3'
+            4'h4 : out = 8'b10011001; // '4'
+            4'h5 : out = 8'b10010010; // '5'
+            4'h6 : out = 8'b10000010; // '6'
+            4'h7 : out = 8'b11111000; // '7'
+            4'h8 : out = 8'b10000000; // '8'
+            4'h9 : out = 8'b10010000; // '9'
+            4'ha : out = 8'b10001000; // 'A'
+            4'hb : out = 8'b10000011; // 'b'
+            4'hc : out = 8'b10100111; // 'c'
+            4'hd : out = 8'b10100001; // 'd'
+            4'he : out = 8'b10000110; // 'E'
+            4'hf : out = 8'b10001110; // 'F
+            default : out = 8'b11111111;
+        endcase
+    end
+    
+endmodule
